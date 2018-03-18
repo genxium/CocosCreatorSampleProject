3f0e6562474ff251088cba43c2d61e1ec4772d5e
Wing <genxium@hotmail.com>
2018-03-19 00:07:39 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+This project serves as a sample project for https://shimo.im/docs/1khN38OavAgFTyK1.
---
+++ assets/scripts/Star.js
@@ -0,0 +1,62 @@
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // When the distance between the star and main character is less than this value, collection of the point will be completed
+        pickRadius: 0,
+        // The game object
+        game: {
+            default: null,
+            serializable: false
+        }
+    },
+
+    getPlayerDistance: function () {
+        // judge the distance according to the position of the player node
+        var playerPos = this.game.player.getPosition();
+        // calculate the distance between two nodes according to their positions
+        var dist = cc.pDistance(this.node.position, playerPos);
+        return dist;
+    },
+
+    onPicked: function() {
+        // when the stars are being collected, invoke the interface in the Game script to generate a new star
+        this.game.spawnNewStar();
+        // invoke the scoring method of the Game script
+        this.game.gainScore();
+        // then destroy the current star's node
+        this.node.destroy();
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    update: function (dt) {
+        // judge if the distance between the star and main character is shorter than the collecting distance for each frame
+        if (this.getPlayerDistance() < this.pickRadius) {
+            // invoke collecting behavior
+            this.onPicked();
+            return;
+        }
+
+        // update the transparency of the star according to the timer in the Game script
+        var opacityRatio = 1 - this.game.timer/this.game.starDuration;
+        var minOpacity = 50;
+        this.node.opacity = minOpacity + Math.floor(opacityRatio * (255 - minOpacity));
+    },
+});
